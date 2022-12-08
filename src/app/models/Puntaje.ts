dfeae33444e396fe01911de9e3b7ca38d45024ce
Agustin Romero <agusromero0815@gmail.com>
2022-12-08 10:51:12 -0300

    actualizando models

--- src/app/models/Puntaje.ts
+++ src/app/models/Puntaje.ts
@@ -4,6 +4,11 @@ export default (sequelize: Sequelize) => {
   sequelize.define(
     'Puntaje',
     {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
       puntaje: {
         type: DataTypes.FLOAT,
         allowNull: false
