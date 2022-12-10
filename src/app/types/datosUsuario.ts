ac00b74e5f4d79ea7605572b33c09c41a5d57029
Agustin Romero <agusromero0815@gmail.com>
2022-12-10 11:46:31 -0300

    creación de un nuevo type para usuarios

---
+++ src/app/types/datosUsuario.ts
@@ -0,0 +1,9 @@
+export default interface datosUsuario {
+  email: string,
+  clave: string,
+  nombre_completo: string,
+  numero_celular: string,
+  direccion: string,
+  dni: string,
+  tipo: number
+}
