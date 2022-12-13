bf06d5fa0d290f1157cba5a37f1a47d9723d9d19
Agustin Romero <agusromero0815@gmail.com>
2022-12-13 11:16:04 -0300

    modificación de Checkout User, Login y Register

--- src/app/services/Login.service.ts
+++ src/app/services/Login.service.ts
@@ -1,19 +1,32 @@
 import { sign } from 'jsonwebtoken';
+import datosUsuario from '../types/datosUsuario';
 import { searchUser } from './CheckoutUser.service'; 
 
 export const loginUser = async ({ email, clave}: {email: string, clave: string}) => {
   if(!email || !clave) throw { error: 406, message: 'Faltan parámetros' };
 
-  const findUser = await searchUser(email, clave);
+  const { 
+    nombre_completo, 
+    id, 
+    dni, 
+    numero_celular, 
+    direccion,
+    tipo
+  }: datosUsuario = await searchUser(email, clave);
 
   const token = sign({
-    email: findUser.email,
+    email,
     clave
   }, String(process.env.SECRET));
 
   return {
+    id,
+    nombre_completo,
     email,
-    nombre_completo: findUser.nombre_completo,
+    numero_celular,
+    dni,
+    direccion,
+    tipo,
     token
   };
 }
