ecce5d404c210d92e20abf2b4e5eedbe7b9c8db8
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2022-12-21 12:10:49 -0300

    service y ruta get cantidad de campings por provincia

--- src/app/routes/Provincia.route.ts
+++ src/app/routes/Provincia.route.ts
@@ -1,7 +1,18 @@
 import { Router, Request, Response } from 'express';
-import { getProvincias } from '../services/Provincias.service';
+import { getProvincias, getProvinciasCantCampings } from '../services/Provincias.service';
 
 const ProvinciaRouter: Router = Router();
+
+
+ProvinciaRouter.get('/campings', async (_req: Request, res: Response) => {
+
+  try {
+    res.status(200).json(await getProvinciasCantCampings())
+  } catch {
+    res.status(404).json({error: 'no se pudo en http://localhost/api/provincias/campings'});
+  }
+});
+
 
 ProvinciaRouter.get('/:idPais', async (req: Request<{idPais: string}>, res: Response) => {
   const { idPais } = req.params;
