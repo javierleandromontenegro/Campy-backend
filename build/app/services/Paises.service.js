73201ae66d10714c28b79328aad9b0850b22b32f
javier montenegro <inversiones.vvm@gmail.com>
2022-12-22 11:24:29 -0300

    build eliminado

--- build/app/services/Paises.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPaises = void 0;
-const { Paises } = require("../db");
-const getPaises = async () => await Paises.findAll({
-    attributes: ['id', 'nombre', 'imagen']
-});
-exports.getPaises = getPaises;
