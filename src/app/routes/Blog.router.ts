5aa9823845a1677ac5a5f1a72e20731afa2c7fd5
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2022-12-28 18:02:18 -0300

    rutas de updatePost y updateComentario

--- src/app/routes/Blog.router.ts
+++ src/app/routes/Blog.router.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
 import { datosPost, datosComentario } from '../types/datosBlog';
-import { postBlogComentario, postBlogCreate, getPostImagenes, getAllPost, getComentario, getPostPorId } from '../services/Blog.service';
+import { postBlogComentario, postBlogCreate, getPostImagenes, getAllPost, getComentario, getPostPorId, updatePost, updateComentario } from '../services/Blog.service';
+import { checkoutUser, /* checkoutAdmin */ } from '../services/CheckoutUser.service';
 
 const BlogRouter: Router = Router();
 
-BlogRouter.post('/create', async (req: Request<datosPost>, res: Response) => {
+BlogRouter.post('/create', checkoutUser, async (req: Request<{}, datosPost>, res: Response) => {
 
     try {
       res.status(200).json(await postBlogCreate(req.body))
@@ -15,7 +16,7 @@ BlogRouter.post('/create', async (req: Request<datosPost>, res: Response) => {
 });
 
 
-BlogRouter.post('/comentario', async (req: Request<datosComentario>, res: Response) => {
+BlogRouter.post('/comentario', checkoutUser, async (req: Request<{}, datosComentario>, res: Response) => {
 
     try {
       res.status(200).json(await postBlogComentario(req.body))
@@ -63,6 +64,26 @@ BlogRouter.get('/:idPost', async (req: Request<{idPost: number}>, res: Response)
     res.status(200).json(await getPostPorId(idPost))
   } catch {
     res.status(404).json({error: `no se pudo en http://localhost/api/blog/${idPost}`});
+  }
+});
+
+BlogRouter.put('/:idPost', async (req: Request<{idPost: number}>, res: Response) => {
+  const { idPost } = req.params;
+
+  try {
+    res.status(200).json(await updatePost(req.body, idPost))
+  } catch {
+    res.status(404).json({error: `no se pudo en http://localhost/api/blog/${idPost}`});
+  }
+});
+
+BlogRouter.put('/comentarios/:idComentario', async (req: Request<{idComentario: number}>, res: Response) => {
+  const { idComentario } = req.params;
+
+  try {
+    res.status(200).json(await updateComentario(req.body, idComentario))
+  } catch {
+    res.status(404).json({error: `no se pudo en http://localhost/api/blog/comentarios/${idComentario}`});
   }
 });
 
