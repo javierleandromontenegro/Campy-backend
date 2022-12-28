0fb28d7087ad585b450d13eec7ce7c2ce126b785
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2022-12-28 15:27:08 -0300

    rutas de getAllPost, getComentario y getPostPorId

--- src/app/routes/Blog.router.ts
+++ src/app/routes/Blog.router.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { datosPost, datosComentario } from '../types/datosBlog';
-import { postBlogComentario, postBlogCreate, getPostImagenes } from '../services/Blog.service';
+import { postBlogComentario, postBlogCreate, getPostImagenes, getAllPost, getComentario, getPostPorId } from '../services/Blog.service';
 
 const BlogRouter: Router = Router();
 
@@ -35,6 +35,35 @@ BlogRouter.get('/imagenes/:idPost', async (req: Request<{idPost: number}>, res: 
         console.log(error);
       res.status(error.error).json(error);
     }
+});
+
+BlogRouter.get('/', async (_req: Request, res: Response) => { 
+  try {
+    res.status(200).json(await getAllPost())
+  } catch {
+    res.status(404).json({error: `no se pudo en http://localhost/api/blog`});
+  }
+});
+
+BlogRouter.get('/comentarios/:idPost', async (req: Request<{idPost: number}>, res: Response) => {
+  const { idPost } = req.params;
+
+  try {
+    res.status(200).json(await getComentario(idPost))
+  } catch(error) {
+    console.log(error)
+    res.status(404).json({error: `no se pudo en http://localhost/api/blog/comentarios/${idPost}`});
+  }
+});
+
+BlogRouter.get('/:idPost', async (req: Request<{idPost: number}>, res: Response) => {
+  const { idPost } = req.params;
+
+  try {
+    res.status(200).json(await getPostPorId(idPost))
+  } catch {
+    res.status(404).json({error: `no se pudo en http://localhost/api/blog/${idPost}`});
+  }
 });
 
 
