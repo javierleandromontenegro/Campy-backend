946771a4ec6dc5679c83a1fb775bb754c007ef7c
Santiago Pagnanelli <109765245+SantiagoPag@users.noreply.github.com>
2022-12-28 15:26:19 -0300

    service de getAllPost, getComentario x id de post y getPost x id

--- src/app/services/Blog.service.ts
+++ src/app/services/Blog.service.ts
@@ -1,8 +1,8 @@
-import { datosComentario, datosPost } from "../types/datosBlog";
+import { datosAllPost, datosComentario, datosPost } from "../types/datosBlog";
 
 const { sequelize } = require("../db");
 
-export const postBlogCreate = async ({titulo, texto, imagenes}: datosPost): Promise<number> => {
+export const postBlogCreate = async ({titulo, texto, imagenes, usuarioId}: datosPost): Promise<number> => {
   
     if (!titulo || !texto || !imagenes) throw {
       error: 406,
@@ -10,7 +10,8 @@ export const postBlogCreate = async ({titulo, texto, imagenes}: datosPost): Prom
     };
 
     const [postBlogId]: [postBlogId: number] = await sequelize.query(
-        `INSERT INTO Posts_usuarios (titulo, texto, fecha, createdAt, updatedAt, UsuarioId) VALUES ('${titulo.replace("'", "\\'")}', '${texto.replace("'", "\\'")}', NOW(), NOW(), NOW(), 42)`
+        `INSERT INTO Posts_usuarios (titulo, texto, fecha, createdAt, updatedAt, UsuarioId) 
+        VALUES ('${titulo.replace("'", "\\'")}', '${texto.replace("'", "\\'")}', NOW(), NOW(), NOW(), ${usuarioId})`
     )
 
     console.log(postBlogId);
@@ -28,7 +29,7 @@ export const postBlogCreate = async ({titulo, texto, imagenes}: datosPost): Prom
   }
 
 
-  export const postBlogComentario = async ({comentario}: datosComentario): Promise<number> => {
+  export const postBlogComentario = async ({comentario, usuarioId, postId}: datosComentario): Promise<number> => {
   
     if (!comentario) throw {
       error: 406,
@@ -36,7 +37,7 @@ export const postBlogCreate = async ({titulo, texto, imagenes}: datosPost): Prom
     };
 
     const [postBlogComentario] = await sequelize.query(
-        `INSERT INTO Posts_comentarios (comentario, createdAt, updatedAt, UsuarioId, PostUsuarioId) VALUES ('${comentario.replace("'", "\\'")}', NOW(), NOW(), 42)`
+        `INSERT INTO Posts_comentarios (comentario, createdAt, updatedAt, UsuarioId, PostsUsuarioId) VALUES ('${comentario.replace("'", "\\'")}', NOW(), NOW(), ${usuarioId}, ${postId})`
     )
 
     console.log(postBlogComentario);
@@ -51,3 +52,40 @@ export const postBlogCreate = async ({titulo, texto, imagenes}: datosPost): Prom
     
     return querySql.map((query: any): string => query.url);
   }
+
+  export const getAllPost = async (): Promise<datosAllPost[]> => {
+    const [querySql]: [querySql: datosAllPost[]] = await sequelize.query(
+      `SELECT PU.titulo, U.username, PU.fecha, PU.texto FROM Posts_usuarios as PU INNER JOIN Usuarios as U ON U.id=PU.UsuarioId`
+    );
+  
+    return querySql;
+  }
+
+  export const getComentario = async (id: number): Promise<string[]> => {
+    const [querySql]: [querySql: datosPost[]] = await sequelize.query(
+      `SELECT U.username, PC.comentario, PC.createdAt
+      FROM Posts_comentarios AS PC
+      INNER JOIN Usuarios AS U ON U.id=PC.UsuarioId
+      INNER JOIN Posts_usuarios AS PU ON PU.id=PC.PostsUsuarioId
+      WHERE PU.id=${id}`
+    );
+      
+    return querySql.map((query: any): string => query);
+  }
+
+  export const getPostPorId= async (id: number): Promise<datosPost> => {
+  const [querySql]: [querySql: datosPost[]] = await sequelize.query(
+    `SELECT PU.id, U.username, PU.fecha, PU.titulo, PU.texto
+    FROM Posts_usuarios AS PU
+    INNER JOIN Usuarios AS U ON U.id=PU.UsuarioId
+    WHERE PU.id=${id}`
+  );
+
+  if (!querySql[0]) throw { error: 404, message: 'No se encontró un post con ese ID' };
+
+  querySql[0].imagenes = await getPostImagenes(querySql[0].id);
+  
+  querySql[0].comentarios = await getComentario(querySql[0].id);
+
+  return querySql[0];
+}
